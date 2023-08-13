ed7101613575b512d9dbda8d09162fb758fb61a5
Shyam Prasad Rimal <140776897+shyamprasadrimal5@users.noreply.github.com>
2023-08-13 13:03:15 +0545

    ShyamPrasadRimal_Day1_Repository
    ShyamPrasadRimal_Day1_Repository

---
+++ JavaScript-Export.js
@@ -0,0 +1,15 @@
+const student_details = {
+    name: "Shyam",
+    age: 26
+}
+
+
+const nepal = function() { //Expression Function
+    console.log("this is a fun function");
+}
+
+//commonjs format
+module.exports = {
+    america: student_details,
+    china: nepal
+}
---
+++ NodeJs-Import.js
@@ -0,0 +1,4 @@
+const importedstudent_details = require("./JavaScript-Export");
+console.log("the imported object is ", importedstudent_details);
+console.log("the object inside importedstudent_details::", importedstudent_details.america);
+importedstudent_details.china();
